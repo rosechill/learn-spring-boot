d513ae941d6fb98920ba1ee1a9bf882b90acb655
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-06-20 13:33:29 +0700

    init project

---
+++ learn-spring-validation/src/main/java/jiwon/learn_spring_validation/LearnSpringValidationApplication.java
@@ -0,0 +1,13 @@
+package jiwon.learn_spring_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringValidationApplication.class, args);
+	}
+
+}
