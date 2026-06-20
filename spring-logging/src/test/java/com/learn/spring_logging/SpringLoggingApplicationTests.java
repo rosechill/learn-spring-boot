6d54bd49ec04d3f5357f7dd8891dd1411086b71d
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-06-20 12:16:26 +0700

    initalize project

---
+++ spring-logging/src/test/java/com/learn/spring_logging/SpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
