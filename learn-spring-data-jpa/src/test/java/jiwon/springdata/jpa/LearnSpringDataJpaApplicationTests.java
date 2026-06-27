1042b7d7e0a6cb8f5ba260dc9bd8cfc8e3f08902
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-06-27 13:27:47 +0700

    init repo spring data jpa

---
+++ learn-spring-data-jpa/src/test/java/jiwon/springdata/jpa/LearnSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwon.springdata.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
