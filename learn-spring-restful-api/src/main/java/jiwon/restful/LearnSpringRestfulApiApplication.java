b8fdd65f016893a19a6395a68cdd5b0a4a9d8c89
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-06-28 16:22:33 +0700

    init learn spring restful api

---
+++ learn-spring-restful-api/src/main/java/jiwon/restful/LearnSpringRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package jiwon.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringRestfulApiApplication.class, args);
+	}
+
+}
---
+++ learn-spring-restful-api/src/test/java/jiwon/restful/LearnSpringRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwon.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
