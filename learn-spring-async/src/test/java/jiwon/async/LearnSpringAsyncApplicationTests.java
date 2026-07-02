2a1e976103232d24da7e4d2128af6cc07b42f7ba
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-07-02 20:50:32 +0700

    init spring async

---
+++ learn-spring-async/src/test/java/jiwon/async/LearnSpringAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwon.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
