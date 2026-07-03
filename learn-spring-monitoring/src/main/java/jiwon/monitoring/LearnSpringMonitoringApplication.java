82e3ec2cabfe29bdbbc377b9209e7c40f8f061f4
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-07-03 14:48:31 +0700

    init spring monitoring

---
+++ learn-spring-monitoring/src/main/java/jiwon/monitoring/LearnSpringMonitoringApplication.java
@@ -0,0 +1,13 @@
+package jiwon.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringMonitoringApplication.class, args);
+	}
+
+}
